3bb8eca5fd3c3bfafa1f08b821e98a1e613cbb4e
Mazharul Haque <emailtomazharul@gmail.com>
2022-12-02 20:59:00 +0600

    skyakash personal-website

--- assets/js/main.js
+++ assets/js/main.js
@@ -211,6 +211,31 @@
 
   });
 
+  window.addEventListener('load', () => {
+    let galleryContainer = select('.gallery-container');
+    if (galleryContainer) {
+      let galleryIsotope = new Isotope(galleryContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+      });
+
+      let galleryFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        galleryFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        galleryIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+      }, true);
+    }
+
+  });
+
   /**
    * Initiate portfolio lightbox 
    */
